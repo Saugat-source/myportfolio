240a941e6e2f4f0efedde2c0ddb8e54314245363
Saugat <saugatanisha@Saugats-MacBook-Pro.local>
2021-03-28 12:00:27 +0545

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+    return (
+        <div>
+            <h2 className="text-primary">Welcome to my portfolio//..\\</h2>
+        </div>
+    )
+}
+
+export default App;
